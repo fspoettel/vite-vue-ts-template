e7514707447ffc59b7b8f193439898a6ac246848
Felix Spöttel <1682504+fspoettel@users.noreply.github.com>
2023-12-29 23:36:50 +0100

    chore(deps): update tooling dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,14 @@
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import autoprefixer from "autoprefixer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   css: {
     postcss: {
-      plugins: [require("autoprefixer")],
+      plugins: [autoprefixer()],
     },
   },
   test: {
